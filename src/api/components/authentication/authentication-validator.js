b8dfe97c903eb7214ecd6b4700f03a51d519bf09
melfanny <melfanny.535230152@stu.untar.ac.id>
2024-04-10 01:15:26 +0700

    Exercise Project API

--- src/api/components/authentication/authentication-validator.js
+++ src/api/components/authentication/authentication-validator.js
@@ -1,10 +1,38 @@
 const joi = require('joi');
 
 module.exports = {
-  login: {
+  createUser: {
     body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
       email: joi.string().email().required().label('Email'),
-      password: joi.string().required().label('Password'),
+      password: joi.string().min(6).max(32).required().label('Password'),
+      password_confirm: joi
+        .string()
+        .min(6)
+        .max(32)
+        .required()
+        .label('password_confirm'),
+    },
+  },
+
+  updateUser: {
+    body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
+      email: joi.string().email().required().label('Email'),
+    },
+  },
+
+  // validator buat changepassword
+  changePassword: {
+    body: {
+      oldPassword: joi.string().required().label('OldPassword'),
+      newPassword: joi.string().min(6).max(32).required().label('NewPassword'),
+      confirmPassword: joi
+        .string()
+        .min(6)
+        .max(32)
+        .required()
+        .label('Confirm Password'),
     },
   },
 };
